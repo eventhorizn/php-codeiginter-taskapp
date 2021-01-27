105d954af693d5672fc82884f4dc02026a7101a6
Gary Hake <Gary.Hake@waystar.com>
2021-01-26 20:51:57 -0500

    mvc basics: controllers, views, layouts

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -2,11 +2,11 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Tasks extends BaseController
 {
 	public function index()
 	{
-		return view("Home/index");
+		return view("Tasks/index");
 	}
 
 	//--------------------------------------------------------------------
